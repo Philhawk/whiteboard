6d7ab135513d34557a70e9c97636eed1ad6e855f
Philhawk <pkjacob@gmail.com>
2016-10-07 11:52:40 -0400

    working whiteboard

--- browser/app.js
+++ browser/app.js
@@ -1,6 +1,16 @@
+var socket = io(window.location.origin);
+
 window.whiteboard.on('draw', function(start, end, strokeColor) {
   if (strokeColor === undefined){
     strokeColor = 'black'
   }
-  console.log(start, end, strokeColor)
+  socket.emit('drawing', start, end, strokeColor)
+  socket.on('drew', function(start, end, strokeColor){
+    console.log(start, end, strokeColor)
+    window.whiteboard.draw(start, end, strokeColor)
+  })
 })
+
+socket.on('connect', function () {
+    console.log('I have made a persistent two-way connection to the server!');
+});
--- server.js
+++ server.js
@@ -6,7 +6,26 @@ var server = http.createServer();
 var express = require('express');
 var app = express();
 
+var socketio = require('socket.io');
+
 server.on('request', app);
+
+var io = socketio(server);
+
+io.on('connection', function (socket) {
+    console.log('A new client has connected!');
+    console.log(socket.id);
+
+    socket.on('disconnect', function(){
+      console.log('disconnected');
+    })
+
+    socket.on('drawing', function(start, end, strokeColor){
+      socket.broadcast.emit('drew', start, end, strokeColor)
+    })
+});
+
+
 
 server.listen(1337, function () {
     console.log('The server is listening on port 1337!');
